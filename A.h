48a6bd6df2d2f0cada6f2d23192e011b0dd7710f
Cotton-Cloud-s <48150041+Cotton-Cloud-s@users.noreply.github.com>
2019-03-15 15:53:01 +0000

    Add files via upload

---
+++ A.h
@@ -0,0 +1,116 @@
+﻿#pragma once
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Lector 
+{
+public:
+	void Greet_Audience(string a) 
+	{
+		a = LectorName;
+	}
+	void Give_A_Speech(string a) 
+	{
+		cout << a;
+	}
+	void setSci_Field(string a) 
+	{
+		Sci_Field = a;
+	}
+	void setLectorName(string b) 
+	{
+		LectorName = b;
+	}
+	void setExperience(int c) 
+	{
+		Experience = c;
+	}
+	void setEdDegree(string d)
+	{
+		EdDegree = d;
+	}
+	string getLectorName() 
+	{
+		return LectorName;
+	}
+private:
+	string Sci_Field;
+	string LectorName;
+	int Experience;
+	string EdDegree;
+};
+
+class Audience {
+public:
+	void InterruptLect(string w) 
+	{
+		cout << " " << w << "";
+	}
+	void setGoOut(bool f)
+	{
+		Like = f;
+		if (Like == 0) cout << "";
+	}
+private:
+	bool Like;
+};
+
+class Staff
+{
+protected:
+	int number_staff;
+	string color;
+public:
+	Staff()
+	{
+		color = "a";
+		number_staff = 0;
+	}
+	Staff(const Staff &obj)
+	{
+	}
+	Staff(string color, int number_staff)
+	{
+		this->number_staff = number_staff;
+		this->color = color;
+	}
+	
+	virtual void getRotation()
+	{
+		cout << "Staff changed. " << endl;
+	}
+	virtual void help()
+	{
+		cout << "Volunteers helped listeners take their seats. " << endl;
+	}
+};
+
+class Security : public Staff
+{
+private:
+public:
+	Security(string color, int number_staff) :Staff(color, number_staff)
+	{
+	}
+	void CheckInvitation()
+	{
+		cout << "All invitations were checked but some were false so some people were not allowed. " << endl;
+	}
+
+	virtual void getRotation()
+	{
+		cout << "Staff changed. " << "Security is provided. " << endl;
+	}
+};
+class Volunteers :public Staff
+{
+public:
+	Volunteers(string color, int mumber_staff) : Staff(color, number_staff)
+	{
+	}
+	virtual void help()
+	{
+		cout << "Volunteers helped listeners take their seats. " << endl;
+	}
+};
---
+++ Source3.cpp
@@ -0,0 +1,44 @@
+﻿#include "A.h"
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+
+void funcShow(Staff object)
+{}
+Staff funcReturnObject()
+{
+	Staff object;
+	return object;
+}
+int main() {
+	setlocale(LC_ALL, "Russian");
+	Lector AKlymenko;
+	AKlymenko.setLectorName("Алла Клименко");
+	AKlymenko.setSci_Field("Счастье в жизни людей");
+	AKlymenko.setExperience(5);
+	AKlymenko.setEdDegree("Бакалавр в области изучения психологии");
+	string speech1 = "1991.txt";
+	ifstream fin;
+	fin.open(speech1);
+	char st;
+	Security Ohorona("Black", 6);
+	Ohorona.getRotation();
+	Ohorona.CheckInvitation();
+	Volunteers Help("Blue", 15);
+	Help.help();
+	while (fin.get(st))
+	{
+		cout << st;
+	}
+	Staff obj1;
+	funcShow(obj1);
+	funcReturnObject();
+	Staff obj2 = obj1;
+	const int Lenght = 3;
+	Staff arr[Lenght];
+	arr[0] = Staff("Black", 10);
+	fin.close();
+	system("pause");
+	return 0;
+}
